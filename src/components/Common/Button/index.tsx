ee24dc5b78b091ef4e311df2466f6124b2defb3b
Zong Xian <48787208+kangzongxian@users.noreply.github.com>
2024-12-22 14:53:11 +0800

    Implement common theme styles in frontend
    Implement common theme styles in frontend

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import './index.css';
-
+import { Button as AntButton, theme } from 'antd';
+const { useToken } = theme;
 interface ButtonProps {
   children?: React.ReactNode;
   onClick?: () => void;
   disabled?: boolean;
   className?: string;
-  type?: 'button' | 'submit' | 'reset';
+  type?: 'primary' | 'default' | 'dashed' | 'text' | 'link';
 }
-
 const Button: React.FC<ButtonProps> = ({
   onClick,
   children,
   disabled,
-  className = '',
-  type = 'button',
-}) => (
-  <button
+  type = 'primary',
+}) => {
+  const { token } = useToken();
+
+  return (
+  <AntButton
+    size="large"
+    style={{
+      backgroundColor: token.colorPrimary,
+      color: 'white',
+    }}
+    onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#db2a6b')} // Hover background color
+    onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = '#c81f58')} // Revert to original
     type={type} // eslint-disable-line react/button-has-type
-    className={`custom-button font-base ${className}`}
     onClick={onClick}
     disabled={disabled}
   >
     {children}
-  </button>
-);
+  </AntButton>
+)
+}
 
 export default Button;
--- src/pages/ContactUsPage/index.tsx
+++ src/pages/ContactUsPage/index.tsx
@@ -38,7 +38,7 @@ const ContactUsPage: React.FC = () => {
         <input type="email" name="email" placeholder="Email" required autoComplete="off" />
       </div>
       <textarea name="message" placeholder="Write your inquiry..." required autoComplete="off" />
-        <Button type="submit">Send Message</Button>
+        <button className='custom-button' type="submit">Send Message</button>
         {submitted && <p>Thank you! Your message has been sent.</p>}
       </form>
     </div>
