8f8099b420bef8b983a2e903c0f9a3a044ecd64e
Zong Xian <48787208+kangzongxian@users.noreply.github.com>
2024-12-25 23:01:37 +0800

    Update culture card
    Update culture card

--- tests/cultures.test.ts
+++ tests/cultures.test.ts
@@ -1,21 +1,30 @@
 import { test, expect } from '@playwright/test';
 import { baseUrl } from '../playwright.config';
 
-
 test('Cultures Page has KathakalAI Button in title', async ({ page }) => {
-    await page.goto("/cultures")
-    await expect(page.getByRole('button', { name: 'KathakalAI' })).toBeVisible()
+  await page.goto('/cultures');
+  await expect(page.getByRole('button', { name: 'KathakalAI' })).toBeVisible();
 });
 
+test('Clicking on Kathakali Read More brings us to Kathakali Information page', async ({
+  page,
+}) => {
+  await page.goto('/cultures');
+  await expect(
+    page
+      .locator('div')
+      .filter({ hasText: /^PreviewKathakaliREAD MORE →$/ })
+      .getByRole('button'),
+  ).toBeVisible();
+  await page
+    .locator('div')
+    .filter({ hasText: /^PreviewKathakaliREAD MORE →$/ })
+    .getByRole('button')
+    .click();
+  await expect(page.getByText('Kathakali', { exact: true })).toBeVisible();
 
-test('Clicking on Kathakali Read More brings us to Kathakali Information page', async ({ page }) => {
-    await page.goto("/cultures")
-    await expect(page.getByRole('button', { name: 'Kathakali Kathakali READ MORE' }).getByRole('button')).toBeVisible()
-    await page.getByRole('button', { name: 'Kathakali Kathakali READ MORE' }).getByRole('button').click()
-    await expect(page.getByText('Kathakali', { exact: true })).toBeVisible()
-
-    // Check url is /cultures/kathakali
-    expect(page.url()).toBe(`${baseUrl}/cultures/kathakali`)
+  // Check url is /cultures/kathakali
+  expect(page.url()).toBe(`${baseUrl}/cultures/kathakali`);
 });
 
 // TODO: Test able to upload Character Recognition File
