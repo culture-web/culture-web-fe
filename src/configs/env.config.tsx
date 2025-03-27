5673c5bb30969df2530c55889dc010890ebc8837
Zong Xian <48787208+kangzongxian@users.noreply.github.com>
2025-03-27 20:07:05 +0800

    Update env.config.tsx

--- src/configs/env.config.tsx
+++ src/configs/env.config.tsx
@@ -1,9 +1,5 @@
 const backendUri = process.env.VITE_BACKEND_URI;
 
-if (!backendUri) {
-  process.exit(1);
-}
-
 const BACKEND_URI = backendUri;
 
 export const EMAIL_API_KEY = process.env.VITE_EMAIL_API_KEY;
