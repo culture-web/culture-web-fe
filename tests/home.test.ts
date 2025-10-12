e59cbef4e47cb025afae93174a042ba470785b92
Gerald Ng Jun Xiang <geraldngjx@gmail.com>
2025-10-12 17:34:26 +0800

    test: fix playwright tests

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -8,8 +8,8 @@ test('Homepage has KathakalAI Button in title', async ({ page }) => {
 
 test('Clicking on Learn More About Other Cultures brings us to cultures', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('button', { name: 'Learn More About Other Cultures' })).toBeVisible();
-  await page.getByRole('button', { name: 'Learn More About Other Cultures' }).click();
+  await expect(page.getByRole('button', { name: 'Explore Cultures' })).toBeVisible();
+  await page.getByRole('button', { name: 'Explore Cultures' }).click();
   await expect(page.getByRole('heading', { name: 'Cultures' })).toBeVisible();
 
   // Check url is /cultures
@@ -19,9 +19,9 @@ test('Clicking on Learn More About Other Cultures brings us to cultures', async 
 test('Clicking on Learn More About KathakalAI brings us to About Us', async ({ page }) => {
   await page.goto('/');
   await expect(
-    page.getByRole('button', { name: 'Learn More About KathakalAI' }),
+    page.getByRole('button', { name: 'Learn About Us' }),
   ).toBeVisible();
-  await page.getByRole('button', { name: 'Learn More About KathakalAI' }).click();
+  await page.getByRole('button', { name: 'Learn About Us' }).click();
   await expect(page.getByRole('heading', { name: 'About Us' })).toBeVisible();
 
   // Check url is /about-us
